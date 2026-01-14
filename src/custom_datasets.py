4c5baec8d4e12e0f178462d2c051f0f79eb016e8
Marcin Weglowski <marcin.weglowski1@gmail.com>
2026-01-14 21:56:06 +0100

    lightgbm implementation, cross-validation, sophisticated experiment details saving

--- src/custom_datasets.py
+++ src/custom_datasets.py
@@ -1,4 +1,3 @@
-import torch
 import os
 from torch.utils.data import Dataset
 from PIL import Image
@@ -11,12 +10,6 @@ from torch.utils.data import Dataset
 
 class SkinLesionDataset(Dataset):
     def __init__(self, dataframe, root_dir, transforms=None):
-        """
-        Args:
-            dataframe (pd.DataFrame): The dataframe object (train_df, val_df, etc.)
-            root_dir (string): Directory with all the images (the flat folder).
-            transform (callable, optional): Optional transform to be applied.
-        """
         self.dataframe = dataframe
         self.root_dir = root_dir
         self.transforms = transforms
@@ -25,11 +18,8 @@ class SkinLesionDataset(Dataset):
         return len(self.dataframe)
 
     def __getitem__(self, index):
-        # Use iloc to access by integer position, regardless of the DataFrame index
         row = self.dataframe.iloc[index]
         
-        # Column 0 is the filename, Column 1 is the label
-        # Adjust these keys if your CSV columns have specific names like 'image_id'
         img_name = os.path.join(self.root_dir, f'{row.iloc[0]}.jpg') 
         label = int(row.iloc[1])
         
